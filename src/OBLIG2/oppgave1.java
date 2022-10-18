df1d740e27574cd99931e0c9d482cdc438f7b623
Fahadfrj <112732586+Fahadfrj@users.noreply.github.com>
2022-10-18 19:57:43 +0200

    d

---
+++ src/OBLIG2/oppgave1.java
@@ -0,0 +1,44 @@
+package OBLIG2;
+import static javax.swing.JOptionPane.*;
+public class oppgave1 {
+
+    public static void main(String [] args){
+
+        int o= -1,n=0,sum=0;
+
+
+
+
+        while(o<n){
+            try{
+                o = Integer.parseInt(showInputDialog("Enter the first number"));
+
+            } catch (Exception e){
+                o =0;
+            }
+            try{
+                n = Integer.parseInt(showInputDialog("Enter the second number"));
+
+            } catch (Exception e){
+                n =0;
+            }
+            if(o<n){
+
+              showMessageDialog(null, "The first number should be higher than the second one");
+            }
+
+
+        }
+        for(int i=n ; i<=o; i++){
+            if(i==o){
+                sum+=i;
+                System.out.print(" "+i + " = " + sum);
+                continue;
+            }
+            System.out.print(" "+i + " +");
+            sum+=i;
+        }
+
+
+    }
+}
